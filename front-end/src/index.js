f339e93d7d2b479d7c4692da246fb76eb411b261
caren <carenoliveirasilvapontes@gmail.com>
2023-03-29 07:38:46 -0300

    feat:adicionando rotas e provider no index

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Provider from './context/Provider';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider>
     <App />
+    </Provider>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
