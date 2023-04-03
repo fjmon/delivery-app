43f85be31d41a0785b3210fe77a24cf5c373b083
Henrik <henrik.ruan4@gmail.com>
2023-04-03 19:50:08 -0300

    feat: getSellers

---
+++ back-end/src/services/sellerService.js
@@ -0,0 +1,10 @@
+const { User } = require('../database/models'); 
+
+const getSellers = async () => {
+  const sellers = await User.findAll({ where: { role: 'seller' } });
+  return sellers;
+};
+
+module.exports = {
+  getSellers,
+};
