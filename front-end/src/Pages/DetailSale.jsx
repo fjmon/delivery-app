abc1ec3ce04100df95b5f1fffb2691b7696c98bc
Henrik Santos <henrik.ruan4@gmail.com>
2023-04-05 19:06:22 -0300

    Update DetailSale.jsx

--- front-end/src/Pages/DetailSale.jsx
+++ front-end/src/Pages/DetailSale.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+{/*import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavBarProducts from '../Components/NavBarProducts';
 import { getData } from '../hooks/useLocalStorage';
@@ -80,3 +80,4 @@ export default function DetailSale(props) {
     </>
   );
 }
+*/}
