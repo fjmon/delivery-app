9566c762b0316d22a3d57903f494f98d66134a11
Henrik Santos <henrik.ruan4@gmail.com>
2023-04-12 16:40:42 -0300

    feat: css seller orders

--- front-end/src/Components/NavBarSeller.jsx
+++ front-end/src/Components/NavBarSeller.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-max-depth */
+/* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import { getData } from '../hooks/useLocalStorage';
@@ -5,6 +7,7 @@ import { getData } from '../hooks/useLocalStorage';
 export default function NavBarSeller() {
   const history = useHistory();
   const [name, setName] = useState('');
+  const [showNavLinks, setShowNavLinks] = useState(false);
 
   const handleExit = () => {
     localStorage.clear();
@@ -23,19 +26,67 @@ export default function NavBarSeller() {
   };
 
   return (
-    <nav>
-      <div>
-        <h3>
-          <Link data-testid={ ROUTE_ELEMENTS[2] } to="/seller/orders">Pedidos</Link>
-        </h3>
-        <h3 data-testid={ ROUTE_ELEMENTS[3] }>{ name }</h3>
+    <nav
+      className="bg-white border-gray-200 dark:bg-gray-900 sticky top-0 w-full mb-4"
+    >
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <h1 className="self-center text-2xl md:text-4xl cursor-pointer font-semibold whitespace-nowrap dark:text-white">
+          DELI
+          <span className="text-amber-400 underline">BEER</span>
+          Y 🍺
+        </h1>
         <button
+          data-collapse-toggle="navbar-default"
           type="button"
-          data-testid={ ROUTE_ELEMENTS[4] }
-          onClick={ handleExit }
+          className={ `
+          ${showNavLinks ? 'hidden' : 'block'}
+          inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600` }
+          aria-controls="navbar-default"
+          aria-expanded="false"
+          onClick={ () => setShowNavLinks((prev) => !prev) }
         >
-          Sair
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
         </button>
+        <div
+          className={ `
+          ${showNavLinks ? 'block' : 'hidden'} md:block w-64 md:w-auto text-right md:text-xl" id="navbar-default` }
+        >
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+            onClick={ () => setShowNavLinks((prev) => !prev) }
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
+          </button>
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-900 md:dark:bg-gray-900 dark:border-gray-700">
+            <Link
+              className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              data-testid={ ROUTE_ELEMENTS[2] }
+              to="/seller/orders"
+            >
+              Pedidos
+            </Link>
+            <h1
+              className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:cursor-pointer"
+              data-testid={ ROUTE_ELEMENTS[3] }
+            >
+              { name }
+            </h1>
+            <button
+              className="text-right block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              type="button"
+              data-testid={ ROUTE_ELEMENTS[4] }
+              onClick={ handleExit }
+            >
+              Sair
+            </button>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- front-end/src/Pages/SellerOrders.jsx
+++ front-end/src/Pages/SellerOrders.jsx
@@ -31,32 +31,63 @@ export default function SellerOrders() {
   return (
     <>
       <NavBarSeller />
-      {
-        (pedidos.map((e) => (
-          <Link to={ `/seller/orders/${e.id}` } key={ e.id }>
-            <p>Pedido</p>
-            <p data-testid={ `seller_orders__element-order-id-${e.id}` }>{e.id}</p>
-            <p
-              data-testid={ `seller_orders__element-delivery-status-${e.id}` }
-            >
-              {e.status}
-            </p>
-            <p
-              data-testid={ `seller_orders__element-order-date-${e.id}` }
-            >
-              {new Date(e.saleDate).toLocaleDateString('pt-BR')}
-            </p>
-            <p
-              data-testid={ `seller_orders__element-card-price-${e.id}` }
-            >
-              {e.totalPrice.replace('.', ',')}
-            </p>
-            <p data-testid={ `seller_orders__element-card-address-${e.id}` }>
-              {`${e.deliveryAddress}, ${e.deliveryNumber}`}
-            </p>
-          </Link>
-        )))
-      }
+      { pedidos.length > 0 ? (
+        <>
+          <h1 className="w-11/12 md:w-6/12 text-4xl mx-auto mt-0 md:mt-10">Pedidos:</h1>
+          <div className="w-10/12 md:w-6/12 mx-auto flex justify-start flex-wrap mt-3">
+            {(pedidos.map((e) => (
+              <Link
+                to={ `/seller/orders/${e.id}` }
+                key={ e.id }
+                className="
+                m-3 relative shadow border rounded border-black p-3 w-full md:w-72
+                hover:bg-slate-100
+                "
+              >
+                <p
+                  data-testid={ `customer_orders__element-order-id-${e.id}` }
+                  className="text-lg"
+                >
+                  {' Pedido: 000'}
+                  {e.id}
+                </p>
+                <p
+                  data-testid={ `seller_orders__element-delivery-status-${e.id}` }
+                  className={ `
+                  ${e.status === 'Pendente' ? 'text-red-500' : ''}
+                  ${e.status === 'Em Trânsito' ? 'text-yellow-500' : ''}
+                  ${e.status === 'Preparando' ? 'text-yellow-600' : ''}
+                  ${e.status === 'Entregue' ? 'text-green-400' : ''}
+                  absolute top-3 right-3
+                  ` }
+                >
+                  {'Status: '}
+                  {e.status}
+                </p>
+                <p
+                  data-testid={ `seller_orders__element-order-date-${e.id}` }
+                >
+                  {'Data: '}
+                  {new Date(e.saleDate).toLocaleDateString('pt-BR')}
+                </p>
+                <p
+                  data-testid={ `seller_orders__element-card-price-${e.id}` }
+                >
+                  Total: R$
+                  {e.totalPrice.replace('.', ',')}
+                </p>
+                <p data-testid={ `seller_orders__element-card-address-${e.id}` }>
+                  {`${e.deliveryAddress}, ${e.deliveryNumber}`}
+                </p>
+              </Link>
+            )))}
+          </div>
+        </>
+      ) : (
+        <div className="mx-auto mt-80 text-center align-middle">
+          <h1 className="text-3xl">Nenhum pedido encontrado.</h1>
+        </div>
+      ) }
     </>
   );
 } // teste
